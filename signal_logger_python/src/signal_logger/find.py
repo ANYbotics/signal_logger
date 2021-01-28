4eb4f859cc1955078e3185fff9a76807e44b4c47
Stéphane Caron <scaron@anybotics.com>
2021-01-28 09:27:49 +0000

    Merge branch 'fix/silo_ui_folder' into 'master'
    [silo_ui] Open last-modified file from folder
    
    Closes #UNKNOWN
    
    GitOrigin-RevId: 1e52bdf2153cda1c4e82e08929dedd51843f251f

--- signal_logger_python/src/signal_logger/find.py
+++ signal_logger_python/src/signal_logger/find.py
@@ -15,6 +15,20 @@ def custom_formatwarning(msg, *args, **kwargs):
 
 warnings.formatwarning = custom_formatwarning
 
+def pick_last_modified(files):
+    """
+    Pick last modified file in a list.
+
+    :param files: List of files.
+    :return: Last modified among them.
+    """
+    last_modified = max(files, key=os.path.getmtime)
+    if len(files) > 1:
+        warnings.warn(
+            "Several candidate log files: {}".format(files) + "\n"
+            "Picking last modified: '{}'".format(last_modified))
+    return last_modified
+
 
 def find_log(fpath, logdir=None):
     """
@@ -26,8 +40,11 @@ def find_log(fpath, logdir=None):
     """
     if os.path.isfile(fpath):
         return fpath
+    if os.path.isdir(fpath):
+        logdir = fpath
+        fpath = ''
 
-    # Look for log file in ~/.ros
+    # Look for log file in logdir or else in ~/.ros
     if logdir is None:
         logdir = os.path.expanduser("~") + "/.ros/"
     if logdir[-1] != '/':
@@ -35,19 +52,18 @@ def find_log(fpath, logdir=None):
     if os.path.isfile(logdir + fpath):
         return logdir + fpath
 
-    # If argument is a prefix, return most recent log
-    prefix_files = glob.glob(logdir + fpath + '*[0-9][0-9][0-9].silo')
-    if prefix_files:
-        return max(prefix_files, key=os.path.getctime)
+    # Try fpath as a prefix
+    if len(fpath) > 0:
+        prefix_files = glob.glob(logdir + fpath + '*[0-9][0-9][0-9].silo')
+        if prefix_files:
+            return pick_last_modified(prefix_files)
 
-    # If argument is a suffix, warn and ask the user
+    # Try fpath as a suffix
     suffix = fpath + ".silo"
-    candidates = [logdir + f for f in os.listdir(logdir) if f.endswith(suffix)]
-    if not candidates:
-        warnings.warn("Found no log file for '{}'".format(fpath))
-        return fpath
-    if len(candidates) > 1:
-        warnings.warn(
-            "Several log file matches in {}".format(candidates) + "\n"
-            "Picking '{}'".format(fpath))
-    return candidates[0]
+    suffix_files = [logdir + f for f in os.listdir(logdir) if f.endswith(suffix)]
+    if suffix_files:
+        return pick_last_modified(suffix_files)
+
+    # Well, we tried all we could ;)
+    warnings.warn("Found no log file for '{}'".format(fpath))
+    return fpath
