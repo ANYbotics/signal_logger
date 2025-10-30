1cc1ae6b5bf7ee3dca03be0a6a4b0af3c03cb32d
Marco Caselli <mcaselli@anybotics.com>
2025-10-30 17:33:51 +0000

    Merge branch 'feature/policy_tester' into 'master'
    Poc locomotion policy evaluator
    
    GitOrigin-RevId: e1d0e487e56931176e031e7f6abc0d21427dbf1f

--- signal_logger_python/src/signal_logger/__init__.py
+++ signal_logger_python/src/signal_logger/__init__.py
@@ -8,13 +8,17 @@
 # Adapted from __init__.py (license: BSD)
 # Source: https://github.com/jrl-umi3218/mc_rtc/tree/master/utils/mc_log_gui
 
+from .cartesian_signal import CartesianSignal
 from .find import find_log
 from .read import LogReader
+from .rotation_signals import QuaternionSignal
 from .signal import Signal
 from .silo import Silo
 
 __all__ = [
+    'CartesianSignal',
     'LogReader',
+    'QuaternionSignal',
     'Signal',
     'Silo',
     'find_log',
